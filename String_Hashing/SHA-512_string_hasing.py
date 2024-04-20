ac167fcb63d9fb8c5bddb3b893caac3b8c3434dc
NaseebGrewal <ngrewal240@gmail.com>
2024-04-20 15:11:37 +0200

    updated ruff formatting

--- String_Hashing/SHA-512_string_hasing.py
+++ String_Hashing/SHA-512_string_hasing.py
@@ -1,10 +1,12 @@
 # Hashing is irreversible
 # only way to know which string was used is to brute force
-import hashlib  
-  
-def hash_string(input_string):  
-    sha_signature = hashlib.sha512(input_string.encode()).hexdigest()  
-    return sha_signature  
-  
-# Test the function  
-print(hash_string('Hello World'))  
+import hashlib
+
+
+def hash_string(input_string):
+    sha_signature = hashlib.sha512(input_string.encode()).hexdigest()
+    return sha_signature
+
+
+# Test the function
+print(hash_string("Hello World"))
