20ae98995d05f659c4eae7c3dfc472bb33dc1822
NaseebGrewal <ngrewal240@gmail.com>
2024-04-20 15:11:18 +0200

    updated code structure

---
+++ String_Hashing/SHA-512_string_hasing.py
@@ -0,0 +1,10 @@
+# Hashing is irreversible
+# only way to know which string was used is to brute force
+import hashlib  
+  
+def hash_string(input_string):  
+    sha_signature = hashlib.sha512(input_string.encode()).hexdigest()  
+    return sha_signature  
+  
+# Test the function  
+print(hash_string('Hello World'))  
---
+++ string_encrytion_decrytion/strong_encrytion.py
@@ -0,0 +1,25 @@
+from cryptography.fernet import Fernet
+
+# Generate a key
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+
+# Function to encrypt a message
+def encrypt(message):
+    cipher_text = cipher_suite.encrypt(message)
+    return cipher_text
+
+
+# Function to decrypt a message
+def decrypt(cipher_text):
+    plain_text = cipher_suite.decrypt(cipher_text)
+    return plain_text
+
+
+# Test the code
+message = b"HELLO WORLD"
+print("Original message:", message)
+encrypted_message = encrypt(message)
+print("Encrypted message:", encrypted_message)
+print("Decrypted message:", decrypt(encrypted_message))
