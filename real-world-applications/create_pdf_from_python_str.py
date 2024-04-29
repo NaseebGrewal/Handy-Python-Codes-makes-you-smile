39a012650c2000ebf22fed4425855ce65ba2a59d
NaseebGrewal <ngrewal240@gmail.com>
2024-04-29 20:03:52 +0200

    formatted ruff errors

--- real-world-applications/create_pdf_from_python_str.py
+++ real-world-applications/create_pdf_from_python_str.py
@@ -1,19 +1,21 @@
-from fpdf import FPDF  
-  
-def create_pdf(input_string, output_file):  
-    pdf = FPDF()  
-  
-    # Add a page  
-    pdf.add_page()  
-  
-    # Set font  
-    pdf.set_font("Arial", size = 15)  
-  
-    # Add a cell  
-    pdf.cell(200, 10, txt = input_string, ln = True, align = 'C')  
-  
-    # Save the pdf with name .pdf  
-    pdf.output(output_file)  
-  
-# Usage:  
-create_pdf("Hello World", "test.pdf")  
+from fpdf import FPDF
+
+
+def create_pdf(input_string, output_file):
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # Set font
+    pdf.set_font("Arial", size=15)
+
+    # Add a cell
+    pdf.cell(200, 10, txt=input_string, ln=True, align="C")
+
+    # Save the pdf with name .pdf
+    pdf.output(output_file)
+
+
+# Usage:
+create_pdf("Hello World", "test.pdf")
--- real-world-applications/merge_pdf.py
+++ real-world-applications/merge_pdf.py
@@ -1,21 +1,25 @@
-import PyPDF2  
-  
-def merge_pdfs(paths, output):  
-    pdf_writer = PyPDF2.PdfWriter()  
-  
-    for path in paths:  
-        pdf_reader = PyPDF2.PdfReader(path)  
-        for page in range(len(pdf_reader.pages)):  
-            # Add each page to the writer object  
-            pdf_writer.add_page(pdf_reader.pages[page])  
-  
-    # Write out the merged PDF  
-    with open(output, 'wb') as out:  
-        pdf_writer.write(out)  
-  
-if __name__ == '__main__':  
-    # List of PDF files to merge  
-    pdfs = ["list_of_your_pdf_files.... with path, if in same director as code file, then use just name"]  
-      
-    # Output file name  
-    merge_pdfs(pdfs, 'Define the name of your output file.pdf')
+import PyPDF2
+
+
+def merge_pdfs(paths, output):
+    pdf_writer = PyPDF2.PdfWriter()
+
+    for path in paths:
+        pdf_reader = PyPDF2.PdfReader(path)
+        for page in range(len(pdf_reader.pages)):
+            # Add each page to the writer object
+            pdf_writer.add_page(pdf_reader.pages[page])
+
+    # Write out the merged PDF
+    with open(output, "wb") as out:
+        pdf_writer.write(out)
+
+
+if __name__ == "__main__":
+    # List of PDF files to merge
+    pdfs = [
+        "list_of_your_pdf_files.... with path, if in same director as code file, then use just name"
+    ]
+
+    # Output file name
+    merge_pdfs(pdfs, "Define the name of your output file.pdf")
